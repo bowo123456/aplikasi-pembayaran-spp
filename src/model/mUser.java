128394c28787c62f03a0c2b04ea042301ac6e164
bowo123456 <bowo.rezpector5@gmail.com>
2022-07-01 10:46:54 +0700

    update

--- src/model/mUser.java
+++ src/model/mUser.java
@@ -10,6 +10,42 @@ package model;
  * @author Tri Wibowo
  */
 public class mUser {
+ private String pengguna,username,password,level;
+
+    public mUser() {
+    }
+
+    public String getPengguna() {
+        return pengguna;
+    }
+
+    public void setPengguna(String pengguna) {
+        this.pengguna = pengguna;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
  
          
 }
